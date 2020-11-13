5bfddb8ad12e720d603bd42532b9091b6653cedf
goflvhxj1402 <61794054+goflvhxj1402@users.noreply.github.com>
2020-11-13 18:47:16 +0900

    ChessPackage

---
+++ ChessGame작성중/ChessGame/GameMain.cpp
@@ -0,0 +1,74 @@
+#include<windows.h>
+#include"GameManager.h"
+#pragma comment(lib, "msimg32.lib")
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("ChessGame");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPervlnstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, 0, 0, IMAGE_SIZE * 8 + 16, IMAGE_SIZE * 8 + 38, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0))
+	{
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	POINT pt;
+	static GameManager s_GameManager;
+	bool Check;
+	switch (iMessage)
+	{
+	case WM_CREATE:
+		s_GameManager.LoadingGame(hWnd);
+		return 0;
+	case WM_LBUTTONDOWN:
+		pt.x = LOWORD(lParam);
+		pt.y = HIWORD(lParam);
+		Check = s_GameManager.Click(pt);
+		InvalidateRect(hWnd, NULL, true);
+		if (Check == true && s_GameManager.GameCheck(hWnd) == true)
+		{
+			s_GameManager.LoadingGame(hWnd);
+			InvalidateRect(hWnd, NULL, true);
+		}
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		s_GameManager.DrawMap(hdc);
+		s_GameManager.DrawPiece(hdc);
+		s_GameManager.DrawRange(hdc);
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
---
+++ ChessGame작성중/ChessGame/GameManager.h
@@ -0,0 +1,24 @@
+#pragma once
+#include"Player.h"
+
+class GameManager
+{
+private:
+	int m_iTurn;
+	int m_iPlayer, m_iEnemy;
+	bool m_bSelectState;
+	int m_iKingIndex;
+	ImageInfo m_arrMap[Y][X];
+	Player m_arrPlayer[TYPE_MAX];
+public:
+	void LoadingGame(HWND hWnd);
+	void UpdateGame();
+	void DrawMap(HDC hdc);
+	void DrawPiece(HDC hdc);
+	void DrawRange(HDC hdc);
+	bool Click(POINT pt);
+	bool GameCheck(HWND hWnd);
+	GameManager();
+	~GameManager();
+};
+
